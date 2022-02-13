4c67247c90d2dd8ad1ac60fefc28875e4efb793e
Timothy Chin <chinti@bc.edu>
2022-02-13 00:40:31 -0500

    Section 4.2

--- UIViewAnimations/ViewController.swift
+++ UIViewAnimations/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var moveButton: UIButton!
+    @IBOutlet weak var alphaButton: UIButton!
+    @IBOutlet weak var pulseButton: UIButton!
+    @IBOutlet weak var colorChangeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let deviceWidth = view.frame.width
+        imageView.frame.origin.x = deviceWidth
+        alphaButton.frame.origin.x = deviceWidth
+        pulseButton.frame.origin.x = deviceWidth
+        colorChangeButton.frame.origin.x = deviceWidth
 
+    }
+    
+    @IBAction func movePressed(_ sender: UIButton) {
+        imageView.center.x = view.center.x
+        alphaButton.center.x = view.center.x
+        pulseButton.center.x = view.center.x
+        colorChangeButton.center.x = view.center.x
+
+    }
+    
+    @IBAction func alphaPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func pulsePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func colorChangePressed(_ sender: UIButton) {
+    }
 
 }
 
